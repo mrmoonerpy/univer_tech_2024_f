61bf35ce0121041ec1702d6d0ec4f3a2c0e30149
MrMooNeR <mrmooner@vk.com>
2024-02-21 00:07:02 +0300

    homework

--- users.js
+++ users.js
@@ -1,21 +1,31 @@
 async function methodGetUsers() {
   let xhr = new XMLHttpRequest()
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos')
   xhr.send()
+  let xhr_other = new XMLHttpRequest()
+  xhr_other.open('GET', 'https://jsonplaceholder.typicode.com/users')
+  xhr_other.send()
 
   xhr.onload = function () {
     let response = JSON.parse(xhr.response)
+    let response_2 = JSON.parse(xhr_other.response)
+
+    
 
     if (response && Array.isArray(response) && response.length > 0) {
-      response.forEach((user, index) => {
+      response.forEach((user) => {
         let row = '<tr style="border-collapse: collapse;">'
-        row += '<td>' + index + '</td>'
         row += '<td>' + user.id + '</td>'
-        row += '<td>' + user.name + '</td>'
-        row += '<td>' + user.username + '</td>'
-        row += '<td>' + user.email + '</td>'
-        row += '<td>' + user.phone + '</td>'
-        row += '<td>' + user.address.city + '</td>'
+        response_2.forEach((users) => {
+          if (users.id == user.userId) {
+            row += '<td>' + users.name + '</td>'
+          }
+        })
+        row += '<td>' + user.title + '</td>'
+        if (user.completed == true) {
+          row += '<td >' + '<input type="checkbox" id="completed" value=user.completed checked/>' + '</td>'
+        }
+        else {row += '<td >' + '<input type="checkbox" id="completed" value=user.completed/>' + '</td>'}
         row += '</tr > '
 
         $('table tbody').append(row)
